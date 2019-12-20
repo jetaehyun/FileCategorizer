d0a53d5b6b2393207b6cc19215c7348032382b87
jetaehyun <jetaehyun0@gmail.com>
2019-12-20 13:24:25 -0800

    more complete code - can now generate new directories based on file extensions - not tested

--- organize.py
+++ organize.py
@@ -1,6 +1,7 @@
 import os
 
-foldersToMake = {}
+foldersToMake = []
+filesAndExt = {}
 
 def identifyExt(dest):
     extensions = []
@@ -13,13 +14,43 @@ def identifyExt(dest):
         extensions.append(ext)
     return extensions
 
-def populateDictWithExt(ext):
+def populate_foldersToMake(ext):
     for ext1 in range(len(ext)):
-        foldersToMake[ext1] = ext1[1:] # .mp3 = mps
+        foldersToMake.append(ext1[1:]) # ex. mp3
+
+def populate_filesAndExt(files):
+    for file in files:
+        ext = os.path.splitext(file)
+        filesAndExt[file] = ext[1][1:]                          # Ex. value = mp3, key = someMusicFile.mp3
+
+def generateFolderDirectories(dest, ext):
+    addr = os.path.split(dest)
+    for extension in range(len(ext)):
+        newFolder = str(ext[extension]) + '_files'
+        path = os.path.join(addr[0], newFolder)
+        os.mkdir(path)
+
+def OrganizeFiles(addr, files):
+    for file in files:
+        orgFileAddr = os.path.join(addr, file)                  # Ex. /home/user/documents/someMusic.mp3
+        whichFolder = filesAndExt[file]                         # Get extension of file
+        path = os.path.join(addr, whichFolder + '_files', file) # Ex. /home/user/documents/mp3_files/someMusic.mp3
+        if os.path.isdir(path) == False: # only for testing
+            continue 
+        os.rename(orgFileAddr, path)
+
 
 def main():
-    dest = input('Address: ')
-    allFiles = os.listdir(dest)
+    dest = input('Address: ')                                   # Get address
+    if os.path.isdir(dest) == False:
+        print('Invalid Address...')
+        exit()
+    allFiles = os.listdir(dest)                                 # Get list of files
+    populate_filesAndExt(allFiles)                              # Populate Dictionary
+    populate_foldersToMake(identifyExt(allFiles))               # Populate list with unique ext
+    generateFolderDirectories(dest, foldersToMake)              # Make new directories
 
 
-main()
+
+if __name__ == '__main__':
+    main()
